341d92c2fd3811435af362ae23beab864f7a86c3
ropique <ropique.a@gmail.com>
2022-05-25 18:26:32 +0400

    depoying again

--- app/oauth2.py
+++ app/oauth2.py
@@ -37,14 +37,12 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
        
         raise credentials_exception    
-    
-    
+        
     return token_data
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session=  Depends(database.get_db)):
     credentials_exception  = HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, 
     detail  = f"Could not validate credentials", headers = {"WWW-Authenticate": "Bearer"})     
-
 
     token = verify_access_token(token, credentials_exception)
     user = db.query(models.User).filter(models.User.id == token.id).first()
