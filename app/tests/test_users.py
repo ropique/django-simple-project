fd7e30a2d2666e115f5fba8b7e2a18d6ecc5c3af
ropique <ropique.a@gmail.com>
2022-06-01 10:40:04 +0400

    test2

---
+++ app/tests/test_users.py
@@ -0,0 +1,12 @@
+import pytest
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_root(client):
+
+     res = client.get("/")
+     print(res.json().get('message'))
+     assert res.json().get('message') == 'Hello World'
+     assert res.status_code == 200
