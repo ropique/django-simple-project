66d6be9540cff91705edb7947213e74299928570
ropique <ropique.a@gmail.com>
2022-06-02 11:01:11 +0400

    new changes

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, utils 
 from sqlalchemy.orm import  Session 
-from app.database import get_db
+from ..database import get_db
 
 router = APIRouter(
     prefix= "/users",
