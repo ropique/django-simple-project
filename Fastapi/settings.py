59a5b82b8599aef06e376e2ec9be462494617b05
shunna87 <anush_av@instigatemobile.com>
2022-06-27 15:35:50 +0400

    updated settings

--- Fastapi/settings.py
+++ Fastapi/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!i=xngq)eymdw8(tiz8d^d(o)tmp9ch(qgwst#ta2r%n=@6*gt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testing-blog-api.herokuapp.com']
 
 
 # Application definition
