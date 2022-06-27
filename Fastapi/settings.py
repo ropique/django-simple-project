4f559d730b61219481520b94127a13a9bc532f8c
shunna87 <anush_av@instigatemobile.com>
2022-06-27 17:43:33 +0400

    allowed hosts

--- Fastapi/settings.py
+++ Fastapi/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
