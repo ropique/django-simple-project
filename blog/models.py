558d8b6b11600b99b412aba6e99329179aaf9837
shunna87 <anush_av@instigatemobile.com>
2022-06-28 12:10:18 +0400

    max lenght of post content

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length= 250)
-    content = models.TextField(max_length= 5000)
+    content = models.TextField(max_length= 1000)
     date_posted = models.DateTimeField(default= timezone.now)
     category = models.CharField(max_length= 250, default="abbreviations")
     
