f0b0de44d917cf2cc51ecd402e37e9fbfad91465
shunna87 <anush_av@instigatemobile.com>
2022-06-28 12:01:05 +0400

    removed email from register page

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+   
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
+
+
